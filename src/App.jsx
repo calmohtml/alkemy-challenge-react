dd34c6c38dfd0218766273fd6521668895735d30
calmohtml <contactosegovia@protonmail.com>
2022-02-15 14:47:20 -0300

    Starting the form

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,27 @@ import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 
 export default function App() {
+  /*
+  Attention: Only the first query parameter is prefixed with a ? (question mark), all subsequent ones will be prefixed with a & (ampersand). That is how URLs work and nothing related to our API. Here's a full example with two parameters apiKey and includeNutrition: https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true. 
+  */
+
+  // const API_KEY = process.env.REACT_APP_API_KEY;
+  // const url = `https://api.spoonacular.com/recipes/716429/information?apiKey=${API_KEY}`;
+  // https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2
+
+  /* 
+  const getData = async () => {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  getData(); 
+  */
   return (
     <BrowserRouter>
       <Navbar />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,41 @@
 export default function Login() {
+  /*
+    Email: challenge@alkemy.org
+    Password: react
+  */
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    if (email === "" || password === "") {
+      //
+      return alert("Las campos no pueden estar vacíos");
+    } else if (email !== "" && email.includes("@") === false) {
+      //
+      return alert("Debes escribir una dirección de correo electrónico válida");
+    } else if (email !== "challenge@alkemy.org" || password !== "react") {
+      //
+      return alert("Credenciales inválidas");
+    }
+
+    console.log(email, password);
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <form onSubmit={submitHandler}>
+        <span>E-mail:</span>
+        <input type="email" name="email" id="email" />
+        <hr />
+        <span>Contraseña:</span>
+        <input type="password" name="password" id="password" />
+        <hr />
+        <button type="submit">Ingresar</button>
+      </form>
     </div>
   );
 }
